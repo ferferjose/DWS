b2a8510a519f127324408d8d87c467708d852419
ferferjose <147615239+ferferjose@users.noreply.github.com>
2024-10-12 10:43:32 +0200

    Tarea de fechas

---
+++ TAR-06.02 Ejercicio básico de POO (2)/ejercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require_once 'fecha.php';
+        $objFecha = new Fecha(); // instanciamos el objeto de la clase fecha
+        $objFecha->escanearFecha($_GET['fechaUsuario']); //Escanea la fecha y la separa segun el formato
+        //Dice si es bisiesto o no y devuelve true o false y cambia automaticamente los días de febrero
+        if($objFecha->esBisiesto()){
+            echo '<p>Es bisiesto</p>';
+        }else{
+            echo '<p>No es bisiesto</p>';
+        }
+        //Muestra el dia
+        echo $objFecha->getFechaSpliteadaI(2).'/';
+        //Muestra el mes con texto
+        echo $objFecha->getFechasI($objFecha->getMes()).'/';
+        //Muestra el año
+        echo $objFecha->getFechaSpliteadaI(0);
+
+        //Te dice los días que tiene el mes elegido
+        echo '<p>El mes tiene '.$objFecha->getFechasID($objFecha->getMes()).' días</p>';
+        
+    ?>
+</body>
+</html>
---
+++ TAR-06.02 Ejercicio básico de POO (2)/fecha.php
@@ -0,0 +1,124 @@
+<?php
+    Class Fecha{
+        private array $fechas;
+        private array $fechaSpliteada;
+        private int $anio;
+        private int $mes;
+        private int $dia;
+
+        function __construct(){
+            $this->fechas = array(
+                1 => array(
+                    "mes" => "Enero",
+                    "dias" => 31
+                ),
+                2 => array(
+                    "mes" => "Febrero",
+                    "dias" => 28
+                ),
+                3 => array(
+                    "mes" => "Marzo",
+                    "dias" => 31
+                ),
+                4 => array(
+                    "mes" => "Abril",
+                    "dias" => 30
+                ),
+                5 => array(
+                    "mes" => "Mayo",
+                    "dias" => 31
+                ),
+                6 => array(
+                    "mes" => "Junio",
+                    "dias" => 30
+                ),
+                7 => array(
+                    "mes" => "Julio",
+                    "dias" => 31
+                ),
+                8 => array(
+                    "mes" => "Agosto",
+                    "dias" => 31
+                ),
+                9 => array(
+                    "mes" => "Septiembre",
+                    "dias" => 30
+                ),
+                10 => array(
+                    "mes" => "Octubre",
+                    "dias" => 31
+                ),
+                11 => array(
+                    "mes" => "Noviembre",
+                    "dias" => 30
+                ),
+                12 => array(
+                    "mes" => "Diciembre",
+                    "dias" => 31
+                )
+            );
+        }
+        
+
+        //Te cambia automaticamente el dia de febrero si es bisiesto y returnea true o false de si es bisiesto o no
+        function esBisiesto(){
+            if (($this->fechaSpliteada[0] % 4 == 0 && $this->fechaSpliteada[0] % 100 != 0) || $this->fechaSpliteada[0] % 400 == 0){
+                $this->fechas[2]["dias"] = 29;
+                return true;
+            }
+            return false;
+        }
+        function escanearFecha($fechaUsuario){
+            $this->fechaSpliteada=explode('-', $fechaUsuario);
+            $this->anio= (int) $this->fechaSpliteada[0];
+            $this->mes= (int) $this->fechaSpliteada[1];
+            $this->dia= (int) $this->fechaSpliteada[2];
+        }
+
+        ////////////////////////////////////////////////// GETTERS AND SETTER
+        public function getFechaSpliteada()
+        {
+            return $this->fechaSpliteada;
+        }
+        public function getFechaSpliteadaI($i)
+        {
+            return $this->fechaSpliteada[$i];
+        }
+
+        public function setFechaSpliteada($fechaSpliteada)
+        {
+                $this->fechaSpliteada = $fechaSpliteada;
+                return $this;
+        } 
+        public function getFechas()
+        {
+                return $this->fechas;
+        }
+        public function getFechasI($i)
+        {       
+                return $this->fechas[$i]['mes'];
+        }
+        public function getFechasID($i)
+        {       
+                return $this->fechas[$i]['dias'];
+        }
+        public function setFechas($fechas)
+        {
+                $this->fechas = $fechas;
+
+                return $this;
+        }
+        public function getMes()
+        {
+                return $this->mes;
+        }
+        public function setMes($mes)
+        {
+                $this->mes = $mes;
+
+                return $this;
+        }
+    }
+    
+    
+?>
